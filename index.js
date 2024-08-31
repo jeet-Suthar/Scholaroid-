60f2a7fae112058ba68c2a3968acb1a98967669d
jeet-suthar <jitsuthar2003@gmail.com>
2024-08-31 18:00:01 +0530

    standard folder structure

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("<h1> Hello this is home page madafakar</h1>")
+    res.sendFile(__dirname + "/index.html")
 })
 
 app.listen(3000, () => {
